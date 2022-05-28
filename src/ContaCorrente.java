b6285099b63334f22fe759eb75f3b7098836720e
Leonardo Richard <leopm.jus@gmail.com>
2022-05-28 19:04:17 -0300

    Taxa de transferência e saque da conta corrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -6,6 +6,14 @@ public class ContaCorrente extends Conta{
 		super(cliente);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void sacar(double valor) {
+		saldo -= (valor - 0.50) ;
+	}
+	public void transfere(Conta conta, double valor) {
+		saldo -= (valor - 0.70) ;
+		conta.depositar(valor);
+	}
 
 	
 }
